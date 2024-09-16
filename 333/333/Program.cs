4c73fc85aaf4304b6947323cd6337f945dbf5dad
Mikhail.Bobin <bobin.michael@yandex.ru>
2024-09-16 17:18:47 +0500

    Добавил программу сложения чисел

---
+++ 333/333/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _333
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое слагаемое:");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе слагаемое");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Сумма чисел равна " + (x + y));
+            Console.ReadKey();
+        }
+    }
+}
