625209203fd33cbf976e88bca7a682e12c924b97
Mikhail.Bobin <User@PC>
2024-09-30 20:26:49 +0500

    выполнил задание

---
+++ 1 zadanie/1 zadanie/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_zadanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Снова обогнал их");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine();
+            Console.WriteLine("Я снова обогнал их(go-go), снова обогнал их(go - go - go)");
+            Console.WriteLine("Снова обогнал их(go - go), снова обогнал их(go - go - go)");
+            Console.WriteLine("Я снова обогнал их(go-go), снова - снова обогнал(go - go - go)");
+            Console.WriteLine("Я снова обогнал их, я снова обогнал их");
+            Console.WriteLine("Снова обогнал их, снова обогнал их, снова обогнал их");
+            Console.ReadKey();
+
+            Console.ResetColor();
+            Console.Clear();
+            Console.WriteLine("Снова обогнал их");
+            Console.WriteLine();
+            Console.WriteLine("Я снова обогнал их(go-go), снова обогнал их(go - go - go)");
+            Console.WriteLine("Снова обогнал их(go - go), снова обогнал их(go - go - go)");
+            Console.WriteLine("Я снова обогнал их(go-go), снова - снова обогнал(go - go - go)");
+            Console.WriteLine("Я снова обогнал их, я снова обогнал их");
+            Console.WriteLine("Снова обогнал их, снова обогнал их, снова обогнал их");
+            Console.ReadKey();
+        }
+    }
+}
