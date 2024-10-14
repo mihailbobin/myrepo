2f20a8c2543f9aa64255e8e55ea0a83edf3efc97
Mikhail.Bobin <User@PC>
2024-10-14 17:56:17 +0500

    выполнил задание

---
+++ 4 zadanie/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x: ");
+            var x = int.Parse(Console.ReadLine());
+            var y = Math.Sqrt(Math.Pow(2, Math.Pow(x, x)) + Math.Pow(x, Math.Pow(2, x)) + Math.Pow(x, Math.Pow(x, 2)));
+            Console.WriteLine(y);
+            Console.ReadKey();
+        }
+    }
+}
