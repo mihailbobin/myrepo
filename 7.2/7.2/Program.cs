484f9f0b1a65ef6692623e2462728c6de8c7ed0b
Mikhail.Bobin <bobin.michael@yandex.ru>
2024-10-21 16:55:24 +0500

    выполнил 7.2

---
+++ 7.2/7.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x:");
+            var x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y:");
+            var y = int.Parse(Console.ReadLine());
+            Console.WriteLine(IsInArea(x, y));
+            Console.ReadKey();
+        }
+
+        static bool IsInArea(int x, int y)
+        {
+            return (x < -2) && (y > 1);
+        }
+    }
+}
