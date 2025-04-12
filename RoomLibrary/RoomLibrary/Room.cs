a7f2ea1dd1848a69f6981ece536bc4573b650f51
Mikhail.Bobin <User@PC>
2025-04-12 19:32:11 +0500

    выполнил задание

---
+++ RoomLibrary/RoomLibrary/Room.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomLibrary
+{
+    public class Room
+    {
+        public readonly int Number;
+        public int Beds { get; set; }
+        public WindowOrientation Orientation { get; }
+        public decimal PricePerDay { get; set; }
+        public DateTime FreeFrom { get; set; }
+
+        public Room(int number, int beds, WindowOrientation orientation)
+        {
+            Number = number;
+            Beds = beds;
+            Orientation = orientation;
+        }
+
+        public string[] GetInfo()
+        {
+            return new string[]
+            {
+                $"Номер: {Number}, Кроватей: {Beds}",
+                $"Окна: {Orientation}, Цена: {PricePerDay} руб/сутки, Свободен с: {FreeFrom:g}"
+            };
+        }
+    }
+}
